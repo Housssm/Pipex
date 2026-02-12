0496da746c453b8038efcd5122d10d931a0f5133
Houssam El Hariri <hoel-har@f2r11s10.clusters.42paris.fr>
2026-02-12 17:38:11 +0100

    If there is no absolute path it is working

--- main.c
+++ main.c
@@ -6,76 +6,11 @@
 /*   By: hoel-har <hoel-har@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 16:36:10 by marvin            #+#    #+#             */
-/*   Updated: 2026/02/12 14:03:09 by hoel-har         ###   ########.fr       */
+/*   Updated: 2026/02/12 17:37:27 by hoel-har         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-/* int	acces(const char *pathname, int mode)
-{
-	//CHeck if the program can acces the file pathname
-
-	// specifies the accessibility checks to be performed
-	//F_OK WR/W/X_OK pour les autorisations
-	// Tout est OK ---->  returned
-	// ERROR --> nib 1 bit returned
-	// ERROR --> -1 
-}
-
-int	dup2(int oldfd, int newfd)
-{
-	//newfd is a copy of oldfd ---> make the copy
-
-	//oldfd valid fd + newfd == oldfd return newfd
-	//oldfd not valid fd calls fails and newfd NOT CLOSED
-	// Error --> return -1
-
-	//Attention share file offset and file status flags, if onee of them is modified by lseek() the offset also changed for the ohter
-}
-
-int pipe(int pipefd[2])
-{
-	//The array pipefd is used to return two file descriptors referring to the ends of the pipe. 
-	
-
-	//succes --> 0
-	// Error --> -1 and errno set appropriatly
-}
-
-pid_t	fork(void)
-{
-	//create a new process by duplicating the calling process
-
-	// child == parents except for some point becareful to check th exeptions
-}
-
-pid_t	waitpid(pid_t pid, int *status, int options)
-{
-	//call suspends executioin of the calling process until a child SPECIFIED by pid argument has changed state
-	//basically wait oonly for terminated children 
-}
-
-pid_twait(int *status)
-{
-	// like waitpid but untils ONE of its children
-}
-
-int	execve(const char *filename, char *const argv[], char *const envp[])
-{
-	// calling process is replaced by the executed filename
-	//All OK ? --> no return 
-}
-
-int	unlink(const char *pathname)
-{
-	//delete name from file systeme
-	// last link and no process have the fil open ? ---> file deleted and space available to reuse
-	// last link BUT any process still have the file open ---> file remain in existance unti the last fil descripto refering to it is closed
-
-	// success = 0, 
-	//Error = -1 and errno set appropriately
-} */
 
 #include <unistd.h>
 #include <stdio.h>
@@ -163,23 +98,42 @@ int	extract_path(t_data *data, char **full_path, char *av)
 		return (1);
 	return (0);
 }
-
 int	check_existing_path(t_data *data, char *av)
 {
+	char *str;
 	
-	if (ft_strchr(av,'/') && access(av, F_OK | R_OK ) == 0)
+	if (ft_strchr(av,'/'))
 	{
-		data->path = ft_strdup(av);
-		data->cmd[0] = ft_strdup(ft_strrchr(av, '/'));
-		// printf("lasst %s\n",     ft_strrchr(av, '/'));
-		return(0);
+		str = ft_strdup(av);
+		data->cmd = ft_split(str, ' ');
+		if (!data->cmd)
+			return (144);
+		free(str);
+		
 	}
-	else if (ft_strchr(av,'/') && access(av, F_OK | R_OK ) != 0)
-		return (ft_putstr_fd("Invalid Path\n", 1), 1);
-	return(0);
+	// printf("%s\n", data->cmd[0]);
+	// printf("%s\n", data->cmd[1]);
+	// if (ft_strchr(av,))
+	
+	return (0);
 }
-
-
+// int	check_existing_path(t_data *data, char *av)
+// {
+// 	data->cmd = ft_split(av, ' ');
+// 	if (!data->cmd)
+// 		return (1);
+// 	if (ft_strchr(av,'/') && access(data->cmd[0], F_OK | R_OK ) == 0)
+// 	{
+// 		data->path = ft_strdup(data->cmd[0]);
+// 		if (!data->path)
+// 			return (free_split(data->cmd),2);
+// 		return (0);
+// 	}
+// 	else if (ft_strchr(av,'/') && access(av, F_OK | R_OK ) != 0)
+// 		return (ft_putstr_fd("Invalid command", 1), 1);
+// 	return(0);
+// }
+	
 int	check_path(t_data *data, char *av)
 {
 	int		i;
